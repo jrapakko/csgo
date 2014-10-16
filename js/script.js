a27f96e1a6cc1016a631ad19a295d9de4364ab36
Janne Rapakko <janne@rapakko.com>
2014-10-16 13:10:28 -0700

    Added extra disabled styling & changed enabled from link to span (as it uses command option as id)

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
     $('.opt-enable').on('click keydown', 'span', function(event) {
         event.stopPropagation();
         event.preventDefault();
-        $(this).toggleClass('glyphicon-ok glyphicon-remove glyphicon-green glyphicon-red');
-        $(this).parent('.opt-enable').toggleClass('enabled disabled');
+        $(this).toggleClass('enabled disabled glyphicon-ok glyphicon-remove glyphicon-green glyphicon-red');
+        $(this).parents('tr').find('td > code').toggleClass('text-muted');
     });
 });
