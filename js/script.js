8e321d0df6aed6165de4b9ab7b2b08656a00218a
Janne Rapakko <janne@rapakko.com>
2014-10-16 13:30:37 -0700

    updated enable/disable script to handle keyboard input (enter)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 jQuery(document).ready(function($) {
     
-    $('.opt-enable').on('click keydown', 'span', function(event) {
-        event.stopPropagation();
-        event.preventDefault();
-        $(this).toggleClass('enabled disabled glyphicon-ok glyphicon-remove glyphicon-green glyphicon-red');
-        $(this).parents('tr').find('td > code').toggleClass('text-muted');
+    $('.opt-enable').on('click', 'span', function(event) {
+        return toggleCfgCommand(this, event)
+    });
+    $('.opt-enable').keydown(function(event) {
+        if ( event.which == 13 ) {
+            return toggleCfgCommand($(this).children('span'), event)
+        }
     });
 });
+
+function toggleCfgCommand(obj, event) {
+    event.stopPropagation();
+    event.preventDefault();
+    $(obj).toggleClass('enabled disabled glyphicon-ok glyphicon-remove glyphicon-green glyphicon-red');
+    $(obj).parents('tr').find('td > code').toggleClass('text-muted');
+    return false;
+}
