03c3c50b7be037f37ade659d73169e0fb4837c31
Janne Rapakko <janne@rapakko.com>
2014-10-17 07:49:14 -0400

    fixed link for $.get for file template

--- js/autoexec-strings.js
+++ js/autoexec-strings.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    $.get('/autoexec/autoexec_template.html', function(data) {
+    $.get('/csgo/autoexec/autoexec_template.html', function(data) {
         // console.log(data);
-        console.log(JSON.parse(data));
+        console.log($.parse(data));
     });
 });
