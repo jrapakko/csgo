15e5ee4ad99c48abde99eefb5c2dccf21c3ba489
Janne Rapakko <janne@rapakko.com>
2016-09-06 02:38:43 -0700

    Added parsing for everything, need to confirm dropdowns working properly & need to implement toggle placeholder testing

--- js/autoexec-strings.js
+++ js/autoexec-strings.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 $('.save-opts').on('click', processSave);
 
 function printAutoExec(obj) {
-    console.log(obj);
+    // console.log(obj);
     str = '';
     str += '//--------------------------------------------------------------------------------------------//\n';
     str += '//                                 AUTOEXEC GENERATED AT:                                       \n';
@@ -32,7 +32,7 @@ function printSection(obj) {
     str += '                              ' + obj.name + '                                \n';
     str += '//--------------------------------------------------------------------------------------------//\n';
     str += '\n';
-    console.log(obj);
+    // console.log(obj);
     for ( opt in obj.opts ) {
         if (obj.opts[opt].enable == true && obj.opts[opt].value != '') {
             str += obj.opts[opt].opt + ' = ' + obj.opts[opt].value + ';\n'
@@ -52,8 +52,7 @@ function processSave(event) {
         console.log(jsonObj);
         // console.log($.parseJSON(data));
         // console.log(printAutoExec($.parseJSON(data)));
-        var vars = $('#rate, #netgraph, #video, #audio, #mouse, #game, #viewmodel, #hud, #xhair, table#keybinds, #misc');
-        console.log(vars);
+        var vars = $('table#rate, table#netgraph, table#video, table#audio, table#mouse, table#game, table#viewmodel, table#hud, table#xhair, table#keybinds, table#misc');
         vars.each(function() { parseValues(jsonObj, this) });
 
         var downloadText = printAutoExec(jsonObj);
@@ -68,17 +67,32 @@ function processSave(event) {
 
 function parseValues(jsonObj, section) {
     var name = $(section).attr('id');
+
+    // slider parsing
     var sliderVars = $('tr.slider', section);
+    sliderVars.each(function() { parseSlider(jsonObj, this, name); });
 
-    sliderVars.each(function() { parseSlider(jsonObj, this, name) });
+    // color slider parsing
+    var colorSliderVars = $('tr.color-slider', section);
+    colorSliderVars.each(function() { parseColorSlider(jsonObj, this, name); });
 
-    // jsonObj[name].opts[];
+    // dropdown & color dropdown parsing
+    var dropdownVars = $('tr.dropdown tr.color-dropdown', section);
+    dropdownVars.each(function() { parseDropdown(jsonObj, this, name); });
+
+    // toggle parsing
+    var toggleVars = $('tr.toggle', section);
+    toggleVars.each(function() { parseToggle(jsonObj, this, name); });
+
+    // text parsing
+    var textVars = $('tr.text', section);
+    textVars.each(function() { parseText(jsonObj, this, name); });
+
 }
 
 function parseSlider(jsonObj, slider, name) {
 
     var cvar = $(slider).attr('id');
-    console.log(cvar);
     // check to see if user has disabled the option
 
     var enable = !$(slider).find('a.opt-enable > span').hasClass('disabled');
@@ -86,6 +100,52 @@ function parseSlider(jsonObj, slider, name) {
     // get the value
     var value = $(slider).find('td > input.sliderValue').val();
 
+    setCvar(jsonObj, cvar, name, enable, value);
+}
+
+function parseColorSlider(jsonObj, slider, name) {
+
+    var cvar = $(slider).attr('id');
+    // check to see if user has disabled the option
+
+    var enable = !$(slider).find('a.opt-enable > span').hasClass('disabled');
+
+    // get the value
+    var value = $(slider).find('td > .color-slider').val();
+
+    setCvar(jsonObj, cvar, name, enable, value);
+}
+
+function parseDropdown(jsonObj, dropdown, name) {
+    var cvar = $(dropdown).attr('id');
+
+    var enable = !$(dropdown).find('a.opt-enable > span').hasClass('disabled');
+
+    var value = $(dropdown).find('td > .dropdown-cvar > button').val();
+
+    setCvar(jsonObj, cvar, name, enable, value);
+
+}
+
+function parseToggle(jsonObj, toggle, name) {
+    var cvar = $(toggle).attr('id');
+    var enable = !$(toggle).find('a.opt-enable > span').hasClass('disabled');
+    var isOff = $(toggle).find('td > .checkbox-inline > div.toggle').hasClass('off');
+    var value = isOff ? 0 : 1;
+    setCvar(jsonObj, cvar, name, enable, value);
+}
+
+function parseText(jsonObj, textObj, name) {
+    var cvar = $(textObj).attr('id');
+    var enable = !$(textObj).find('a.opt-enable > span').hasClass('disabled');
+    var value = $(textObj).find('td > input:text').val();
+    if (value != "") {
+        value = '"' + value + '"';
+    }
+    setCvar(jsonObj, cvar, name, enable, value);
+}
+
+function setCvar(jsonObj, cvar, name, enable, value) {
     // if not user disabled and value is not a placeholder, enable and set the value
     if (enable && value != "") {
         jsonObj[name].opts[cvar].enable = true;
